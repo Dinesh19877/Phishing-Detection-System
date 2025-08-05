a6151ed02a5bb9d55e3c9b0f4496a18d156fabcb
DINESH SHARMA <119472084+Dinesh19877@users.noreply.github.com>
2025-08-05 21:11:11 +0545

    myproject/app directory

--- myproject/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class DomainRank(models.Model):
-    domain_name = models.CharField(max_length=255, unique=True, null=False)
-    rank = models.IntegerField(null=False)
-
-    def __str__(self):
-        return f"{self.domain_name}: {self.rank}"
